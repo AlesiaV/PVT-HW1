8d217bc49e121cc044f5e630414c9e94ba87e2ec
Alesia <36788604+AlesiaV@users.noreply.github.com>
2018-12-07 14:34:32 +0200

    Add files via upload

---
+++ Homework B1-P2,3,4/CheckPhotoCode/CheckPhotoCode/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckPhotoCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Есть ли среди поданных документов на визу Фотография ? " +
+                               "\nЕсли да - введите 1, если нет - введите 0 ");
+
+            int answer1 = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+
+            if (answer1 == 0) { Console.WriteLine("Сделайте новое фото в фотокомнате"); }
+            else
+            {
+                Console.WriteLine("Сделано ли фото более 3-х месяцев назад ? " +
+                       "\nЕсли да - введите 1, если нет - введите 0 ");
+
+
+                int answer2 = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+
+                if (answer2 == 1) { Console.WriteLine("Сделайте новое фото в фотокомнате"); }
+                else
+                {
+                    Console.WriteLine("Использовалось ли это фото на паспорт ? " +
+                             "\nЕсли да - введите 1, если нет - введите 0 ");
+
+                    int answer3 = Convert.ToInt32(Console.ReadLine());
+                    Console.Clear();
+
+                    if (answer3 == 1) Console.WriteLine("Сделайте новое фото в фотокомнате");
+                    else
+                    {
+                        Console.WriteLine("Вклеено ли фото? " +
+                                       "\nЕсли да - введите 1, если нет - введите 0 ");
+
+                        int answer4 = Convert.ToInt32(Console.ReadLine());
+                        Console.Clear();
+
+                        if (answer4 == 0) Console.WriteLine("Вклейте пожалуйста фото и пройдите на оплату");
+
+                        else Console.WriteLine("Пройдите на оплату");
+                    }
+                }
+            } 
+
+
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
