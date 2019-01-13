bfd50d5af8b7a26e8873f2e7b920007fbca77b2f
Alesia <36788604+AlesiaV@users.noreply.github.com>
2019-01-13 18:44:43 +0200

    Add files via upload

---
+++ MusicPlayer (new update)/MusicPlayer (new update)/MusicPlayer/Album.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlayer
+{
+    public class Album
+    {
+        public List<byte> Picture; // обложка альбома
+        public string Name; //название альбома
+        public int Year; //год выпуска альбома
+
+        public Album()
+        {
+            this.Name = "Default album unknown";
+            this.Year = 0;
+        }
+
+        public Album(string name, int year)
+        {
+            this.Name = name;
+            this.Year = year;
+        }
+    }
+}
