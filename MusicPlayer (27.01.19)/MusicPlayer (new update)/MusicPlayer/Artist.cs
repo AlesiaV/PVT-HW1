70216fcd2c7a92ffee1f97fae3484e2f316b2506
Alesia <36788604+AlesiaV@users.noreply.github.com>
2019-01-28 14:45:07 +0200

    Add Generic player; AL6 player1/2 and player2/2

---
+++ MusicPlayer (27.01.19)/MusicPlayer (new update)/MusicPlayer/Artist.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlayer
+{
+    public class Artist
+    {
+        public string Genre; //жанр песен артиста
+        public string Name; //имя артиста или группы
+
+        public Artist()
+        {
+            this.Name = "Default artist unknown";
+            this.Genre = "Default genre unknown";
+        }
+
+        public Artist(string name)
+        {
+            this.Name = name;
+            this.Genre = "Default genre unknown";
+        }
+
+        public Artist(string name, string genre)
+        {
+            this.Name = name;
+            this.Genre = genre;
+        }
+
+        public enum Genres {Undefined, Pop, Disco, PopRock, Metal, Jazz};
+
+        //[Flags]
+        //public enum Genres
+        //{
+        //    Undefined = 0b00000000,
+        //    Pop = 0b00000001,
+        //    Disco = 0b00000010,
+        //    PopRock = 0b00000100,
+        //    Metal = 0b00001000,
+        //    Jazz = 0b00010000,
+        //};
+    }
+}
+
---
+++ MusicPlayer (27.01.19)/MusicPlayer (new update)/MusicPlayer/Player.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MusicPlayer.Extensions;
+using System.Xml.Serialization;
+using System.IO;
+
+
+namespace MusicPlayer
+{
+    public class Player : GenericPlayer<Song>
+    {
+        public Player(ISkin skin) : base(skin)
+        {
+
+        }
+        public override List<Song> Songs { get; set; } = new List<Song>();
+
+        public override bool Play(bool loop = false)
+        {
+            if (_locked)
+            {
+                _skin.Render("The player was locked");
+                return _playing;
+            }
+            else
+            {
+                if (loop)
+                {
+                    _skin.Clear();
+                    _skin.Render($"Player is playing: {Songs[0].Name}, Duration is: {Songs[0].Duration}");
+                }
+                else
+                {
+                    _playing = true;
+                    for (int i = 0; i < Songs.Count; i++)
+                    {
+                        _skin.Clear();
+                        _skin.Render($"Player is playing: {Songs[i].Name}, Duration is: {Songs[i].Duration}");
+                    }
+                }
+                return _playing;
+            }
+        }
+
+        public override void SortByTitle()
+        {
+            List<string> LocalList = new List<string>();
+
+            foreach (var item in Songs)
+            {
+                LocalList.Add(item.Name);
+            }
+
+            LocalList.Sort();
+
+            List<Song> NewSongs = new List<Song>();
+
+            foreach (var item in LocalList)
+            {
+                foreach (var item2 in Songs)
+                {
+                    if (item == item2.Name)
+                        NewSongs.Add(item2);
+                }
+            }
+            Songs = NewSongs;
+        }
+
+        public override void ShowAllListSongs()
+        {
+            _skin.Clear();
+            Console.WriteLine();
+            foreach (var item in Songs)
+            {
+                SongNewShortName(item);
+                _skin.Render($"Name of song: {item.Name}, Genre: {item.Genres}");
+            }
+        }
+
+        public override void ShowAllListSongs(List<Song> songs)
+        {
+            _skin.Clear();
+            Console.WriteLine();
+            foreach (var item in songs)
+            {
+                SongNewShortName(item);
+                _skin.Render($"Name of song: {item.Name}, Genre: {item.Genres}");
+            }
+        }
+
+        private Song SongNewShortName(Song song)
+        {
+            const int LengthOfSongName = 10;
+
+            Song tmpSong = song;
+            tmpSong.Name = tmpSong.Name.ModifiedOfSongsName(LengthOfSongName);
+
+            return tmpSong;
+        }
+
+        public List<Song> FilterByGenre(params Artist.Genres[] genre)
+        {
+            List<Song> newSongs = new List<Song>();
+            foreach (var song in Songs)
+            {
+                foreach (var genreForFilter in genre)
+                {
+                    if (song.Genres == genreForFilter)
+                    {
+                        newSongs.Add(song);
+                    }
+                }
+            }
+
+            return newSongs;
+        }
+
+        public void Clear()
+        {
+            Songs.Clear();
+        }
+
+        public void Load(string folderPath)  //принимает путь к папке
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(folderPath);
+
+            if (directoryInfo.Exists)
+            {
+                var songsFiles = directoryInfo.GetFiles("*.mp3");
+                foreach (var file in songsFiles)
+                {
+                    var musicFile = TagLib.File.Create(file.FullName);
+
+                    Artist artist = new Artist
+                    {
+                        Name = musicFile.Tag.JoinedPerformers,
+                        Genre = musicFile.Tag.JoinedGenres
+                    };
+
+                    Album album = new Album
+                    {
+                        Name = musicFile.Tag.Album,
+                        Year = (int)musicFile.Tag.Year
+                    };
+
+                    Song song = new Song
+                    {
+                        Name = musicFile.Tag.Title,
+                        //Genres = musicFile.Tag.FirstGenre,
+                        Duration = (int)musicFile.Properties.Duration.TotalSeconds,
+                        Artist = artist,
+                        Album = album
+
+                    };
+                    Songs.Add(song);
+                    Console.WriteLine($@"Name song: {song.Name}. Artist: {song.Artist.Name}. Duration: {song.Duration}");
+                }
+
+            }
+
+            else Console.WriteLine("Invalid path to files specified"); //указан неверный путь к файлам
+        }
+
+        public void SaveAsPlaylist()
+        {
+            XmlSerializer serializer = new XmlSerializer(Songs.GetType());
+            using (FileStream fs = new FileStream(@"D:/Музыка/MusicPlaylist.xml", FileMode.OpenOrCreate))
+            {
+                serializer.Serialize(fs, Songs);
+            }
+            Console.WriteLine("Playlist saved");
+        }
+
+        public void LoadPlaylist()
+        {
+            XmlSerializer serializer = new XmlSerializer(Songs.GetType());
+            using (FileStream fs = new FileStream(@"D:/Музыка/MusicPlaylist.xml", FileMode.OpenOrCreate))
+            {
+                Songs = (List<Song>)serializer.Deserialize(fs);
+            }
+        }
+    }
+}
