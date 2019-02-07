aff8f9c98538d2c7eab08e96520d5e678ae834d9
Alesia <36788604+AlesiaV@users.noreply.github.com>
2019-02-07 21:24:16 +0200

    ConsoleImageProcessingApplication

---
+++ Lab/ConsoleImageProcessingApplication/ConsoleImageProcessingApplication/RenameImagesShootDate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+using ConsoleImageProcessingApplication.Extensions;
+
+
+
+namespace ConsoleImageProcessingApplication
+{
+    class RenameImagesShootDate : BasicClass
+    {
+        public RenameImagesShootDate(string patDir, string nameDir) : base(patDir, nameDir)
+        {
+
+        }
+
+        public override void ImageProcessAppl()
+        {
+            foreach (var file in Files)
+            {
+                using (Image image = Image.FromFile(file.FullName))
+                {
+                    //var propItemsDate = image.GetPropertyItem(0x9003);
+                    //string fileName = Encoding.UTF8.GetString(propItemsDate.Value).Replace(':', '.').Substring(0, 16);
+                    string fileName = image.GetFileDate();
+                    file.CopyTo(newPathDirectory + fileName + file.Name);
+                }
+            }
+        }
+    }
+}
---
+++ Lab/ConsoleImageProcessingApplication/ConsoleImageProcessingApplication/SortByYear.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using ConsoleImageProcessingApplication.Extensions;
+
+namespace ConsoleImageProcessingApplication
+{
+    class SortByYear : BasicClass
+    {
+        public SortByYear(string patDir, string nameDir) : base(patDir, nameDir)
+        {
+
+        }
+        public override void ImageProcessAppl()
+        {
+            foreach (var file in Files)
+            {
+                using (Image image = Image.FromFile(file.FullName))
+                {
+                    //var propItemsDate = image.GetPropertyItem(0x9003);
+                    //string fileDate = Encoding.UTF8.GetString(propItemsDate.Value).Replace(':', '.').Substring(0, 4);
+
+                    string fileDate = image.GetFileDate().Substring(0, 4);
+                    
+                    var newDirByYear = new DirectoryInfo(newPathDirectory + fileDate + @"\");
+
+                    if (!newDirByYear.Exists)
+                    {
+                        newDirByYear.Create();
+                    }
+
+                    file.CopyTo(newDirByYear + file.Name);
+                }
+            }
+        }
+    }
+}
